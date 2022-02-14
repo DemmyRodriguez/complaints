2b4859e7f5e6a0d8c2b7bea11c9249c981bbeaf7
DemmyRodriguez <rodriguezdemmy@gmail.com>
2022-02-13 22:47:24 -0500

    Unfinished Complaint App

---
+++ app.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button");
+
+const nypdInput = document.querySelector("input");
+
+const infoDiv = document.querySelector("div");
+
+button.addEventListener('click', async() => {
+let number = nypdInput.value
+
+    // let response = await axios.get(``)
+
+    // infoDiv.innerHTML = ``
+})
