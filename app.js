7ded52018db9a2d21f72c4004af130018d82a15a
DemmyRodriguez <rodriguezdemmy@gmail.com>
2022-02-13 22:49:01 -0500

    trry

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const infoDiv = document.querySelector("div");
 
 button.addEventListener('click', async() => {
 let number = nypdInput.value
-
+  
     // let response = await axios.get(``)
 
     // infoDiv.innerHTML = ``
