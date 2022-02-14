edf672338c2293981c16163f7e901ddee18d3a89
DemmyRodriguez <rodriguezdemmy@gmail.com>
2022-02-14 03:12:25 -0500

    Buttons now work by burough and give description of crimes

--- app.js
+++ app.js
@@ -1,13 +1,141 @@
-const button = document.querySelector("button");
+const BASE_URL = `https://data.cityofnewyork.us/resource/erm2-nwe9.json`
 
-const nypdInput = document.querySelector("input");
+
+const manhattanbtn = document.querySelector("#Manhattan");
+const brooklynbtn = document.querySelector("#Brooklyn");
+const queensbtn = document.querySelector("#Queens");
+const statenbtn = document.querySelector("#Staten");
+const bronxbtn = document.querySelector("#Bronx");
+
+const agency = "NYPD"
+const manhattan = "MANHATTAN";
+const brooklyn = "BROOKLYN";
+const queens = "QUEENS"
+const staten = "STATEN ISLAND";
+const bronx = "BRONX";
+
+const numberInput = document.querySelector("input");
 
 const infoDiv = document.querySelector("div");
 
-button.addEventListener('click', async() => {
-let number = nypdInput.value
-  
-    // let response = await axios.get(``)
+manhattanbtn.addEventListener('click', async() => {
+let number = numberInput.value
 
-    // infoDiv.innerHTML = ``
+    if(number === ""){
+        number = 10;
+    }
+try{
+    let response = await axios.get(`${BASE_URL}?borough=${manhattan}&$limit=${number}&agency=${agency}`)
+    // console.log(response)
+    
+    let answer = response.data
+    console.log(answer)
+
+    for (let i=0; i < answer.length; i++){
+        let answerObj = answer[i].descriptor;
+        // console.log(answerObj)
+        infoDiv.innerHTML += answerObj + "<br>"
+    }
+
+} catch (error) {
+    console.log(error)
+}
+ 
+})
+
+brooklynbtn.addEventListener('click', async() => {
+    let number = numberInput.value
+    
+        if(number === ""){
+            number = 10;
+        }
+    try{
+        let response = await axios.get(`${BASE_URL}?borough=${brooklyn}&$limit=${number}&agency=${agency}`)
+        // console.log(response)
+        
+        let answer = response.data
+        console.log(answer)
+    
+        for (let i=0; i < answer.length; i++){
+            let answerObj = answer[i].descriptor;
+            // console.log(answerObj)
+            infoDiv.innerHTML += answerObj + "<br>"
+        }
+    
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+queensbtn.addEventListener('click', async() => {
+    let number = numberInput.value
+    
+        if(number === ""){
+            number = 10;
+        }
+    try{
+        let response = await axios.get(`${BASE_URL}?borough=${queens}&$limit=${number}&agency=${agency}`)
+        // console.log(response)
+        
+        let answer = response.data
+        console.log(answer)
+    
+        for (let i=0; i < answer.length; i++){
+            let answerObj = answer[i].descriptor;
+            // console.log(answerObj)
+            infoDiv.innerHTML += answerObj + "<br>"
+        }
+    
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+statenbtn.addEventListener('click', async() => {
+    let number = numberInput.value
+    
+        if(number === ""){
+            number = 10;
+        }
+    try{
+        let response = await axios.get(`${BASE_URL}?borough=${staten}&$limit=${number}&agency=${agency}`)
+        // console.log(response)
+        
+        let answer = response.data
+        console.log(answer)
+    
+        for (let i=0; i < answer.length; i++){
+            let answerObj = answer[i].descriptor;
+            // console.log(answerObj)
+            infoDiv.innerHTML += answerObj + "<br>"
+        }
+    
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+bronxbtn.addEventListener('click', async() => {
+    let number = numberInput.value
+    
+        if(number === ""){
+            number = 10;
+        }
+    try{
+        let response = await axios.get(`${BASE_URL}?borough=${bronx}&$limit=${number}&agency=${agency}`)
+        // console.log(response)
+        
+        let answer = response.data
+        console.log(answer)
+    
+        for (let i=0; i < answer.length; i++){
+            let answerObj = answer[i].descriptor;
+            // console.log(answerObj)
+            infoDiv.innerHTML += answerObj + "<br>"
+        }
+    
+    } catch (error) {
+        console.log(error)
+    }
 })
